3f3e4c02de07161f6814afac82ad79ddf335ceef
PascalUlor <pascalulor@yahoo.com>
2019-07-18 15:39:11 +0100

    migrate and seed table data

--- data/migrations/20190718144952_createRecipesIngredients.js
+++ data/migrations/20190718144952_createRecipesIngredients.js
@@ -2,11 +2,17 @@ exports.up = function(knex) {
   return knex.schema
     .createTable("recipes", table => {
       table.increments();
-      table.text("recipename", 128).notNullable();
+      table
+        .text("recipename", 128)
+        .notNullable()
+        .unique();
     })
     .createTable("ingredients", table => {
       table.increments();
-      table.text("ingredientname", 128).notNullable();
+      table
+        .text("ingredientname", 128)
+        .notNullable()
+        .unique();
     });
 };
 
